cfe389e7b4a726d14540abc39a7d41e30eecebda
oqo0 <oq.roman@gmail.com>
2025-01-25 17:26:51 +0300

    Init

---
+++ links.js
@@ -0,0 +1,7 @@
+const links = {
+    screen: "https://example.com/test1",
+    voice: "https://example.com/test2",
+    sfera: "https://example.com/test3",
+    chat: "https://example.com/test4",
+    lgt: "https://example.com/test5",
+};
